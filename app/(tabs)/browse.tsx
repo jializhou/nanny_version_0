06ca7e67070ebe34fce9296561bf5a29b3e6d227
Jiali Zhou <jz2312@nyu.edu>
2025-05-24 13:44:48 +0800

    Add missing Text import

--- app/(tabs)/browse.tsx
+++ app/(tabs)/browse.tsx
@@ -1,3 +1,5 @@
+import { Text, StyleSheet } from 'react-native';
+
 // Previous content omitted for brevity
 // Update the salary display in the caregiver list:
 <Text style={[styles.caregiverRate, { color: colors.primary }]}>
