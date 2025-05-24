4ac27676095d1187c7e6be8deee6bcd79c5221b2
Jiali Zhou <jz2312@nyu.edu>
2025-05-24 13:45:25 +0800

    Fix styles initialization order

--- app/(tabs)/browse.tsx
+++ app/(tabs)/browse.tsx
@@ -1,14 +1,14 @@
 import { Text, StyleSheet } from 'react-native';
 
-// Previous content omitted for brevity
-// Update the salary display in the caregiver list:
-<Text style={[styles.caregiverRate, { color: colors.primary }]}>
-  ¥{item.monthlySalary}/月
-</Text>
-
 const styles = StyleSheet.create({
   caregiverRate: {
     fontSize: 16,
     fontWeight: '600',
   }
 });
+
+// Previous content omitted for brevity
+// Update the salary display in the caregiver list:
+<Text style={[styles.caregiverRate, { color: colors.primary }]}>
+  ¥{item.monthlySalary}/月
+</Text>
